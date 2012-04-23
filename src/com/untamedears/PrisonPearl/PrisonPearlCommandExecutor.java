e82be6d51ab1ad7f7e27f3343d4a5c7d918d2ea5
Matt Thompson <matthewbot@gmail.com>
2012-04-22 21:40:17 -0400

    Added some admin commands and permissions

--- src/com/untamedears/PrisonPearl/PrisonPearlCommandExecutor.java
+++ src/com/untamedears/PrisonPearl/PrisonPearlCommandExecutor.java
@@ -1,5 +1,6 @@
 package com.untamedears.PrisonPearl;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,36 +16,80 @@ public class PrisonPearlCommandExecutor implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		Player player = null;
-		if (sender instanceof Player)
-			player = (Player)sender;
+		if (args.length >= 1) {	
+			if (args[0].equalsIgnoreCase("locate")) {
+				return locate(cmd, sender, args);
+			} else if (args[0].equalsIgnoreCase("free")) {
+				return free(cmd, sender, args);
+			} 
+		}
 		
-		System.out.println("onCommand " + player);
-		if (args.length < 1)
-			return false;
+		sender.sendMessage("Invalid command. Valid commands are locate, free");
+		return true;
+	}
+	
+	private boolean locate(Command cmd, CommandSender sender, String args[]) {
+		String name_posses;
+		String name_possesive;
+		PrisonPearl pp;
 		
-		System.out.println("args[0] " + args[0]);
-		
-		if (args[0].equals("locate") && player != null) {
-			System.out.println("locate");
-			PrisonPearl pp = pearlstorage.getByImprisoned(player);
-			if (pp != null) {
-				String world = pp.getLocation().getWorld().getName();
-				Vector vec = pp.getLocation().toVector();
-				String vecstr = vec.getBlockX() + " " + vec.getBlockY() + " " + vec.getBlockZ();
-				if (pp.getHolder() != null) {
-					sender.sendMessage("Your prison pearl is held by " + pp.getHolderName() + " at " + world + " " + vecstr);
-				} else {
-					sender.sendMessage("Your prison pearl was dropped at " + world + " " + vecstr);
-				}
-			} else {
-				sender.sendMessage("You have no prison pearl!");
+		if (args.length == 1) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Must use /pp locate [playername] at the console");
+				return true;
+			}
+				
+			name_posses = "You have";
+			name_possesive = "Your";
+			pp = pearlstorage.getByImprisoned((Player)sender);
+		} else if (args.length == 2) {
+			if (!sender.hasPermission("prisonpearl.locate")) {
+				sender.sendMessage(cmd.getPermissionMessage());
+				return true;
 			}
 			
+			name_posses = args[1] + " has";
+			name_possesive = args[1] + "'s";
+			pp = pearlstorage.getByImprisoned(args[1]);
+		} else {
+			sender.sendMessage("locate expects 0 or 1 arguments");
 			return true;
 		}
 		
-		return false;
+		if (pp != null) {
+			String world = pp.getLocation().getWorld().getName();
+			Vector vec = pp.getLocation().toVector();
+			String vecstr = vec.getBlockX() + " " + vec.getBlockY() + " " + vec.getBlockZ();
+			if (pp.getHolder() != null) {
+				sender.sendMessage(name_possesive + " prison pearl is held by " + pp.getHolderName() + " at " + world + " " + vecstr);
+			} else {
+				sender.sendMessage(name_possesive + " prison pearl was dropped at " + world + " " + vecstr);
+			}
+		} else {
+			sender.sendMessage(name_posses + " no prison pearl");
+		}
+		
+		return true;
+	}
+	
+	private boolean free(Command cmd, CommandSender sender, String args[]) {
+		if (args.length != 2)
+			return true;
+		
+		if (sender.hasPermission("prisonpearl.free")) {
+			sender.sendMessage(cmd.getPermissionMessage());
+			return true;
+		}
+		
+		PrisonPearl pp = pearlstorage.getByImprisoned(args[1]);
+		if (pp == null) {
+			sender.sendMessage(args[1] + " has no prison pearl");
+			return true;
+		}
+		
+		pearlstorage.free(pp, Bukkit.getWorlds().get(0).getSpawnLocation());
+		sender.sendMessage(args[1] + " is now free");
+		return true;
 	}
 
 }
